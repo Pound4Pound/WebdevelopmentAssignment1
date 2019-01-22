a8911b62a34327291b3d88761890bd358e0782c3
Colin <colin.lee.chee@gmail.com>
2019-01-22 15:07:24 -0700

    added warning to register page to confirm submit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// Javascript added to the Travel website.
+
+var input = document.getElementById ('submit');
+console.log (input);
+
+input.addEventListener ("click", function(event){
+    console.log (event);
+    alert("Are you sure you want to submit?");
+
+} );
+
+
+
+
