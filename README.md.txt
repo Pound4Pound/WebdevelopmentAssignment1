a02f4165b7c92520d071d9cbb4624eca2cdbcddd
Colin Lee-Chee <211367@ACDM.DS.SAIT.CA>
2019-01-27 09:43:14 -0700

    updated README.md

---
+++ README.md.txt
@@ -0,0 +1,19 @@
+Github README.md provides documentation identifying the author, date, course module and “Final Assignment”
+
+
+Author: 		Colin Lee-Chee
+Student ID: 		211367
+
+Course Module:		CPRG 210 -- Web App Dev
+Course Assignment:	Travel Experts Website
+Date:			27 January, 2019
+
+
+Current status:
+
+Program development halted after assignment 6.  A compile error on the creation of the table was noted however the developer did not notice a problem as the page created the table, appended it to the register page and inserted the images and text. Developer thought he achieved the task.
+
+Attempts to develop assignment 7 ran into problems as the code would not compile beyond the table. When attempting to develop the requirement for assignment 7 the web page would not respond and it was identified that the error from appending the table was the issue.  The developer has not been able to resolve the issue for the table.  The error lists:
+_cannot read property 'appendchild' of null_. **error bypassed on 2019/01/27**
+
+**error solution was to assign the DOM element and verify if it exists within the page.**
--- script.js
+++ script.js
@@ -3,45 +3,24 @@
 var input = document.getElementById ('submit');
 console.log (input);
 
+if (input !== null){
+input.addEventListener ("click", function(){
+  console.log (input);
+    alert("Are you sure you want to submit?");
+} 
+)};
 
 
-// input.addEventListener ("click", function(){
-//   console.log (input);
-//     alert("Are you sure you want to submit?");
+input = document.getElementById ('resetForm');
+console.log (input);
 
-// } );
+if (input !== null){
+input.addEventListener ("click", function(event){
+    console.log (event);
+    alert("Are you sure you want to reset?");
 
-
-// input = document.getElementById ('resetForm');
-// console.log (input);
-
-// input.addEventListener ("click", function(event){
-//     console.log (event);
-//     alert("Are you sure you want to reset?");
-
-// } 
-// );
-
- //code to show instructions regarding input when field is focused
-
- var fieldFocus = document.getElementById('name1');
- var fieldInstruction = document.getElementById('firstNameInstr');
- 
- console.log(fieldFocus);
- console.log(fieldInstruction);
- 
- 
-//fieldInstruction.style.display = "block"
-
-// fieldFocus.addEventListener('focus', showInstruction);
-
-// function showInstruction(){
-
-// };
-
-// if (fieldFocus != 'focus'){
-// fieldFocus.removeEventListener('focus', showInstruction);
-// };
+} 
+)};
 
 var images = ['img/harley1.jpg', 
                   'img/Joker2.jpg',
@@ -141,7 +120,7 @@ var webLinks = ['https://www.japan.travel/en/',
 
         // put <table> in the <body>
           contactBody.appendChild(tbl);
-        } ;
+        } else console.log("aaaaa");
 
 
         // tbl border attribute to 
@@ -151,102 +130,30 @@ var webLinks = ['https://www.japan.travel/en/',
       //found an example of table creation on stack overflow
       //was able to modify javascript example by M.A.K. Ripon:
       //https://stackoverflow.com/questions/14643617/create-table-using-javascript
-      //unclear on how to fix  line 108 error: body.appendChild(tbl);
-      //without line 108, table is not created
-
-// });
-
-// if(fieldFocus.addEventListener('focus',function(event)
-// {
-//   //document.getElementById('name1').style.display = block;
-//   fieldInstruction.style.display = "block";
-// }
-
-
-
-// fieldFocus.addEventListener('focus')
-
-
-
-// function myFunction(fieldInstruction) {
-//   fieldInstruction.style.display = "block"
-// }
-
-//fieldInstruction.style.setAttribute('')
-
-
-//imgKitten.setAttribute('src', 'http://placekitten.com/g/600/500'); 
-
-// if (document.myform.firstname.focus()){
-
-//   document.myform.firstNameInstr.style.display = "block";
-// };
-
-
-// var f = function(){
-
-//   var focusField = function(event){
-//     var formInstruction = document.getElementById("firstNameInstr");
-//     formInstruction.style.display = "block";
-//   };
-  
-    
-//   }
-
-
-// focusField.addEventListener('focus', focusField, false);
-// focusField.addEventListener('blur', focusField, false);
-
-
-
-
-      //add a listener to present instructions to fields when focused on
-
-      // var f = function(){
-
-      //   var focusEventHandler = function(event){
-
-      //       var divElement = document.getElementById('credentials')[0];
-      //       divElement.sty
-      //   }
-
-      // }
-
-
-
-
-
-
-
-
-
-
-      // var formInstruction = document.getElementById("name1");
-
-      // formInstruction.addEventListener("onfocus", function(event){
-      //   //focus will display instructions for selected field.
-      //   if( formInstruction)
-      //   {
-      //     firstNameInstr.style.display = "block";
-      //   }
-
-      // })
-
-      // document.myform.name1.onfocus = function(){
-      //   if("focus") {
-      //     firstNameInstr.style.display = "block";
-      //   }  
-
-      // }
-
-   
-
-  
-
       
 
+//code to show instructions regarding input when field is focused
+
+
+var fieldFocus = document.getElementById('name1');
+var fieldInstruction = document.getElementById('firstNameInstr');
+      
+console.log(fieldFocus);
+console.log(fieldInstruction);
+
+if (fieldFocus !== null){
+
+fieldFocus.addEventListener('focus', showInstruction);
 
 
 
+};
 
+function showInstruction(){
+  fieldInstruction.style.display = "block";
+};
 
+// if (fieldFocus !== null){
+// fieldFocus.removeEventListener('focus', showInstruction);
+
+// for (var j = 0; j < images.length; j++) {
